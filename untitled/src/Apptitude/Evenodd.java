922fe02148d17f3af87ac39fafcd4026c4c44005
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-09-02 12:29:21 +0530

    "DSA"

---
+++ untitled/src/Apptitude/Evenodd.java
@@ -0,0 +1,13 @@
+package Apptitude;
+
+public class Evenodd {
+    public static void main(String[] args) {
+        int number = 29;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
